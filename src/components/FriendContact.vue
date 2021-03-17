f1e5a65bf195e0e1134baa536620ec97d8763292
James Muriithi <muriithijames556@gmail.com>
2021-03-18 00:22:00 +0300

    vue components

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,28 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">{{detailsAreVisisble ? 'Hide': 'Show'}}</button>
+    <ul v-if="detailsAreVisisble">
+      <li><strong>Phone: </strong> {{ friend.phone }}</li>
+      <li><strong>Email: </strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: [
+      "friend"
+  ],
+  data() {
+    return {
+      detailsAreVisisble: false,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisisble = !this.detailsAreVisisble;
+    },
+  },
+};
+</script>
